ebbbe7830932a9ab37bdc2cb1649b3f62c8fadcf
sara <sara1992@live.com.pt>
2023-12-26 00:58:56 +0100

    gh pages

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -11,7 +11,7 @@ const DataContext = createContext({});
 
 export const api = {
   loadData: async () => {
-    const json = await fetch("/package.json");
+    const json = await fetch("/events.json");
     return json.json();
   },
 };
